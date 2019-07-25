3547bafdcc178252bda892a90cc52989253930dc
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-07-25 18:08:38 -0300

    Obtendo dados dinamicamente pelo tr e dentro do tr obtendo o td e gravando em uma variável weight e height, criando variavel imc e mudando o textcontent info-imc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-console.log("Fui carregado!!")
 var titulo = document.querySelector("h1");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var patient = document.querySelector("#first-patient");
+
+var tdWeight =  patient.querySelector(".info-peso");
+var weight = tdWeight.textContent;
+
+var tdHeight = patient.querySelector(".info-altura");
+var height = tdHeight.textContent;
+
+var imc = weight / (height * height);
+
+var tdImc = patient.querySelector(".info-imc");
+tdImc.textContent = imc;

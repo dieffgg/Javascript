ce46e17c8446c3ba7509239fb95391287c37c02b
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-07-30 20:29:19 -0300

    Implementando o for e selecionando itens da tabela um por um, delimitando o final da variável com toFixed()

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
 
-var patient = document.querySelectorAll(".patient");
-for(var i = 0; i < patient.length; i++){
-    console.log(patient[i]);
-    
-}
+var patients = document.querySelectorAll(".patient");
+for (var i = 0; i < patients.length; i++){
+        var patient = patients[i]
 
-var tdWeight =  patient.querySelector(".info-peso");
-var weight = tdWeight.textContent;
+        var tdWeight =  patient.querySelector(".info-peso");
+        var weight = tdWeight.textContent;
 
-var tdHeight = patient.querySelector(".info-altura");
-var height = tdHeight.textContent;
+        var tdHeight = patient.querySelector(".info-altura");
+        var height = tdHeight.textContent;
 
-var tdImc = patient.querySelector(".info-imc");
+        var tdImc = patient.querySelector(".info-imc");
 
-var validWeight = true;
-var validHeight = true;
+        var validWeight = true;
+        var validHeight = true;
 
-if(weight <= 0 || weight >= 500){
-    console.log("Peso Inválido");
-    validWeight = false;
-    tdImc.textContent = "Peso inválido";
-}
-if(height >= 4.00 || height <= 1.00){
-    console.log("Altura Inválida");
-    validHeight = false;
-    tdImc.textContent = "Altura inválida";
-}
+        if(weight <= 0 || weight >= 500){
+            console.log("Peso Inválido");
+            validWeight = false;
+            tdImc.textContent = "Peso inválido";
+        }
+        if(height >= 4.00 || height <= 1.00){
+            console.log("Altura Inválida");
+            validHeight = false;
+            tdImc.textContent = "Altura inválida";
+        }
 
-if (validHeight && validWeight){
-    var imc = weight / (height * height);
-    tdImc.textContent = imc;
-}
+        if (validHeight && validWeight){
+            var imc = weight / (height * height);
+            tdImc.textContent = imc.toFixed(2);
+        }
+        
+    }
+
+
 

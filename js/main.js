00cac47944a8da232d63d639268ffdf0b80b17a1
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-08-15 16:53:26 -0300

    Adicionando filhos a tr e passando valores extraidos do form a td nova

--- js/main.js
+++ js/main.js
@@ -46,13 +46,28 @@ function insertButton(event){
     var form = document.querySelector("#form__add")
 
     var name = form.nome.value;
-    var height = form.altura.value;
     var weight = form.peso.value;
+    var height = form.altura.value;
     var fat = form.gordura.value;
 
-    console.log(name);
-    console.log(height);
-    console.log(weight);
-    console.log(fat);
-   
+    var patientTr = document.createElement("tr");
+
+    var nameTd = document.createElement("td");
+    var weightTd = document.createElement("td");
+    var heightTd = document.createElement("td");
+    var fatTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nameTd.textContent = name;
+    weightTd.textContent = weight;
+    heightTd.textContent = height;
+    fatTd.textContent = fat;
+    
+    patientTr.appendChild(nameTd);
+    patientTr.appendChild(nameTd);
+    patientTr.appendChild(weightTd);
+    patientTr.appendChild(heightTd);
+    patientTr.appendChild(fatTd);
+
+    console.log(patientTr)
 }

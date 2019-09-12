251e12d4b908da90a36ea06888b7be00172e640b
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-09-12 16:14:51 -0300

    Animando remoção de pacientes

--- js/remove.js
+++ js/remove.js
@@ -3,5 +3,9 @@ const table = document.querySelector('table');
 table.addEventListener('dblclick',intoEvent)
 
 function intoEvent(event){
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut")
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 1000)
+    
 }

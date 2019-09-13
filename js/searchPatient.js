ac0b306df0f3737c957a523546277cb817805df9
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-09-13 15:41:29 -0300

    Request e response (ajax)

---
+++ js/searchPatient.js
@@ -0,0 +1,15 @@
+const addButton = document.querySelector('#search__patient')
+
+addButton.addEventListener('click', requestResponse1)
+
+function requestResponse1(){
+    console.log('fuiclicado');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', "https://api-pacientes.herokuapp.com/pacientes")
+    xhr.addEventListener('load', function(){
+        console.log(xhr.responseText);  
+    })
+    xhr.send();
+}

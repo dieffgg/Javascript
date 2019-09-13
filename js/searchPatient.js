95843bc059fc4f9023ea4117d29eb19ead3dfdf1
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-09-13 19:20:21 -0300

    Tratando erros com implementação de mensagens

--- js/searchPatient.js
+++ js/searchPatient.js
@@ -2,20 +2,29 @@ const addButton = document.querySelector('#search__patient')
 
 addButton.addEventListener('click', requestResponse1)
 
-function requestResponse1(){
+function requestResponse1() {
     console.log('fuiclicado');
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', "https://api-pacientes.herokuapp.com/pacientes")
-    xhr.addEventListener('load', function(){
-        const response = xhr.responseText
-        const patients = JSON.parse(response)
-        console.log(patients);
+    xhr.open('GET', "https://api-pacientes.herokuapp.com/pacieentes")
+    xhr.addEventListener('load', function () {
 
-        patients.forEach(patient => {
-            addPatientJSON(patient);
-        });
+        const errorajax = document.querySelector('#errorajax')
+        if (xhr.status == 200) {
+            errorajax.classList.add('invisible')
+            const response = xhr.responseText
+            const patients = JSON.parse(response)
+
+            patients.forEach(patient => {
+                addPatientJSON(patient);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            errorajax.classList.remove('invisible')
+        }
+
     })
     xhr.send();
 }
